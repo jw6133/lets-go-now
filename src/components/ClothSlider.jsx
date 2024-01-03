9f5187634728f775ceaa0403aaa21b9c29715d7d
jw6133 <jw6133@naver.com>
2024-01-03 17:11:59 +0900

    hourly forecast success

--- src/components/ClothSlider.jsx
+++ src/components/ClothSlider.jsx
@@ -6,6 +6,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination'; 
 import OpenWeatherDisplay from './OpenWeatherDisplay';
+import styled from 'styled-components';
 
 
 const slider={
@@ -18,6 +19,7 @@ const ClothSlider=()=> {
     const [temperature, setTemperature] = useState(null);
     const [folderName , setFolderName] = useState(null);
     const [item,setItem]= useState(null);
+    const [clothMessage,setClothMessage]=useState('');
 
     const imageCounts = {
         '4' :5,
@@ -33,36 +35,67 @@ const ClothSlider=()=> {
     const getCurrentWeather = (temp) => {
         setTemperature(temp);        
     }
-    // console.log(temperature);
 
     const getImageFolder = (temperature)=>{
         if(temperature<5){
-            return '4'
+            setClothMessage('날씨가 많이 추워요.. 옷을 두껍게 입는걸 추천드릴게요.');
+            return '4';
         }
         else if(temperature >=5 && temperature <9){
-            return '8_5'
+            setClothMessage('슬슬 보온장비를 챙겨야 할 것 같아요.');
+            return '8_5';
         }else if(temperature >= 9 && temperature < 17){
-            return '16_9'
+            setClothMessage('사람에 따라 쌀쌀하다고도 느낄 수 있어요.');
+            return '16_9';
         }
         else if(temperature >= 17 && temperature < 20){
-            return '19_17'
+            setClothMessage('가벼운 긴팔 입기 좋은 날씨에요.');
+            return '19_17';
         }
         else if(temperature >= 20 && temperature < 23){
-            return '20_22'
+            setClothMessage('아주 온난한 날씨에요.');
+            return '20_22';
         }
         else if(temperature >= 23 && temperature < 28){
-            return '27_23'
+            setClothMessage('꽤 더우니 시원하게 입고 나가시는게 좋을 것 같아요.');
+            return '27_23';
         }
         else if(temperature >= 28){
-            return '28'
+            setClothMessage('불볕더위에요.. 얇은 옷차림을 추천드려요.');
+            return '28';
+        }
+        
+    }
+
+    const messageSetter = (temperature)=>{
+        if(temperature<5){
+            setClothMessage('날씨가 많이 추워요.. 옷을 두껍게 입는걸 추천드릴게요.');
+        }
+        else if(temperature >=5 && temperature <9){
+            setClothMessage('슬슬 보온장비를 챙겨야 할 것 같아요.');
+        }else if(temperature >= 9 && temperature < 17){
+            setClothMessage('사람에 따라 쌀쌀하다고도 느낄 수 있어요.');
+        }
+        else if(temperature >= 17 && temperature < 20){
+            setClothMessage('가벼운 긴팔 입기 좋은 날씨에요.');
+        }
+        else if(temperature >= 20 && temperature < 23){
+            setClothMessage('아주 온난한 날씨에요.');
+        }
+        else if(temperature >= 23 && temperature < 28){
+            setClothMessage('꽤 더우니 시원하게 입고 나가시는게 좋을 것 같아요.');
+        }
+        else if(temperature >= 28){
+            setClothMessage('불볕더위에요.. 얇은 옷차림을 추천드려요.');
         }
         
     }
 
     useEffect(()=>{
         if(temperature !== null){
-            const folder = getImageFolder(temperature)
-            setFolderName(folder)
+            const folder = getImageFolder(temperature);
+            setFolderName(folder);
+            messageSetter(temperature);
         }
     },[temperature])
 
@@ -77,11 +110,16 @@ const ClothSlider=()=> {
         }
     }, [folderName]);
     
-    console.log(item)
 
     return (
         <>
             <OpenWeatherDisplay propFunction={getCurrentWeather} />
+            <br/>
+            <br/>
+            <WeatherRecommend>
+                {temperature&&clothMessage}
+            </WeatherRecommend>
+            <br/>
             <Swiper
                 style={slider}
                 slidesPerView={1}
@@ -90,7 +128,7 @@ const ClothSlider=()=> {
                 autoplay={{delay:1000}}
                 spped={1000}
     >
-      {item.map((item, idx) => {
+      {temperature&&item.map((item, idx) => {
           return (
             <SwiperSlide key={idx}>
               <img src={item.src} />
@@ -104,3 +142,7 @@ const ClothSlider=()=> {
 }
 
 export default ClothSlider
+
+const WeatherRecommend= styled.span`
+    
+`
--- src/components/OpenWeatherHour.jsx
+++ src/components/OpenWeatherHour.jsx
@@ -3,6 +3,13 @@ import React, { useEffect, useState } from 'react'
 
 function OpenWeatherHour() {
     const [weatherHour,setWeatherHour]=useState(null);
+    const [weatherHArray,setWeatherHArray]=useState([]);
+    const [filteredList,setFilteredList]=useState([]);
+    const [tempSave,setTempSave]=useState(null);
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, "0")}-${(today.getDate()).toString().padStart(2,"0")} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    console.log(formattedDate);
+
 
     const twoWeatherApi=process.env.REACT_APP_OPENWEATHER_SECOND_API_KEY;
 
@@ -14,21 +21,65 @@ function OpenWeatherHour() {
         });
       },[]);
 
-
     const getHourWeather=async(lat,lon)=>{
         try{
-            const result = await axios.get(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${twoWeatherApi}`);
-            setWeatherHour(result);
+            const result = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${twoWeatherApi}&units=metric`);
+            setWeatherHour(result.data);
+            setWeatherHArray(result.data.list);
         }catch(error){
                 console.error(error)
             }
     }
 
-    // console.log(weatherHour);
+    const getTime=()=>{
+        const filteredArray=[]
+        {weatherHArray&&weatherHArray.map((el)=>{
+                if(el.dt_txt>=formattedDate){
+                    if(filteredArray.length<7){
+                        filteredArray.push(el);
+                    }
+                }
+        })}
+        setFilteredList(filteredArray)
+    }
+
+    useEffect(()=>{
+        getTime();
+    },[weatherHArray])
+
+    // useEffect(()=>{
+    //     const filteredArray=[];
+    //     if(weatherHArray!==null){
+    //         while(cnt<=5){
+    //             if(weatherHArray[hArrayIdx].dt_txt>formattedDate){
+    //                 filteredArray.push(weatherHArray[hArrayIdx]);
+    //                 cnt=cnt+1;
+    //                 hArrayIdx=hArrayIdx+1;
+    //             }
+    //             else{
+    //                 hArrayIdx=hArrayIdx+1;
+    //             }
+    //         }
+    //     }
+    //     hArrayIdx=0;
+    //     setFilteredList(filteredArray);
+    // },[weatherHArray])
+
+    console.log(filteredList)
+    
     return (
-        <div>
-            aa
-        </div>
+        <>
+            <ul>
+                {filteredList&&filteredList.map((el)=>{
+                    return(
+                        <li>
+                            {el.dt_txt} {el.main.temp} {el.weather[0].main}    
+                        </li>
+                    )
+                })}
+            
+        </ul>
+        </>
     )
 }
 
